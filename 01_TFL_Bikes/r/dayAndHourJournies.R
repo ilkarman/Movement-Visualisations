474b8070a3cd67fb99556359b43402663606a787
Ilia Karmanov <ilkarman@microsoft.com>
2016-10-18 15:28:49 +0100

    OSRM for Cycling Directions
    OSRM instead of Bing, also coord.map()

--- 01_TFL_Bikes/r/dayAndHourJournies.R
+++ 01_TFL_Bikes/r/dayAndHourJournies.R
@@ -1,12 +1,9 @@
-# Draft - Ilia Karmanov - 13/10/2016
+# Draft - Ilia Karmanov - 18/10/2016
 #
 # http://cycling.data.tfl.gov.uk/
 #
 # Changes:
-# 1. Use coord_map instead of coord_equal
 # 2. Use gganimate() instead of API call
-# 3. Fix crop, title, etc for animation
-# 4. Error-handling to API download call
 
 library(jsonlite)
 library(dplyr)
@@ -17,6 +14,11 @@ library(stringi)
 library(ggplot2)
 library(grid)
 
+# Routing
+# Use OSRM instead of Bing to get Cycling
+library(osrm)
+options(osrm.server = "http://localhost:5000/")
+
 # Working environment
 cyclePath <- "C:/Users/ilkarman/Desktop/cycleHire/"
 setwd(cyclePath)
@@ -24,7 +26,7 @@ setwd(cyclePath)
 # Constants
 ONLINEPOINTS <- "https://api.tfl.gov.uk/BikePoint?app_id=&app_key="
 ONLINEUSAGE <- "http://cycling.data.tfl.gov.uk/usage-stats/25JourneyDataExtract28Sep2016-04Oct2016.csv"
-BINGKEY <- "REGISTER_WITH_BING"
+#BINGKEY <- "REGISTER_WITH_BING"
 BUILDINGS <- "TQTL_LON"  # https://docs.google.com/uc?id=0B0kRhiw4fD7uQzU3MzBMRzFfSFk&export=download
 
 # Read in Stations
@@ -83,23 +85,32 @@ cycleJourniesSum <- cbind(id=seq(nrow(cycleJourniesSum)), cycleJourniesSum)
 # Get route segments
 # No error-handling ....
 get_route <- function(id, from, to) {
-  URLBASE <- "http://dev.virtualearth.net/REST/V1/Routes/"
-  URLMODE <- "Driving?"
-  URLFROM <- "wp.0="
-  URLTO <- "&wp.1="
-  URLEND <- "&optmz=distance&routeAttributes=routePath&key="
   
-  url.from <- URLencode(from)
-  url.to <- URLencode(to)
+  # BING Directions
+  #URLBASE <- "http://dev.virtualearth.net/REST/V1/Routes/"
+  #URLMODE <- "Driving?"
+  #URLFROM <- "wp.0="
+  #URLTO <- "&wp.1="
+  #URLEND <- "&optmz=distance&routeAttributes=routePath&key="
   
-  sendUrl <- paste0(URLBASE, URLMODE, URLFROM, url.from, URLTO, url.to, URLEND, BINGKEY)
-  resp <- fromJSON(sendUrl)
-  directions <- as.data.frame(resp$resourceSets$resources)
-  journey <- as.data.frame(directions$routePath$line$coordinates)
+  #url.from <- URLencode(from)
+  #url.to <- URLencode(to)
+  
+  #sendUrl <- paste0(URLBASE, URLMODE, URLFROM, url.from, URLTO, url.to, URLEND, BINGKEY)
+  #resp <- fromJSON(sendUrl)
+  #directions <- as.data.frame(resp$resourceSets$resources)
+  #journey <- as.data.frame(directions$routePath$line$coordinates)
+  
+  # Use OSRM instead
+  journey <- osrmRoute(src=from,
+                       dst=to, 
+                       overview= "simplified", 
+                       sp = FALSE)[,c("lat", "lon")]
   
   id_rep <- rep(id,nrow(journey)-1)
   from <- journey[1:nrow(journey)-1,]
   to <- journey[2:nrow(journey),]
+  
   out <- cbind(id_rep, from, to)
   names(out) <- c("id", "from_lat", "from_lon", "to_lat", "to_lon")
   return(out)
@@ -108,16 +119,31 @@ get_route <- function(id, from, to) {
 # API calls
 cycleLegs <- list()
 for (i in 1:nrow(cycleJourniesSum)) {
+  
   # Create route points
-  from <- paste0(cycleJourniesSum[i ,"start.lat"], ",", cycleJourniesSum[i ,"start.lon"])
-  to <- paste0(cycleJourniesSum[i ,"end.lat"], ",", cycleJourniesSum[i ,"end.lon"])
+  
+  #from <- paste0(cycleJourniesSum[i ,"start.lat"], ",", cycleJourniesSum[i ,"start.lon"])
+  #to <- paste0(cycleJourniesSum[i ,"end.lat"], ",", cycleJourniesSum[i ,"end.lon"])
+
+  from <- c(paste0(i, "a"),
+            cycleJourniesSum[i ,"start.lon"],
+            cycleJourniesSum[i ,"start.lat"])
+  to <- c(paste0(i, "b"), 
+          cycleJourniesSum[i ,"end.lon"],
+          cycleJourniesSum[i ,"end.lat"])
+  
   # Get segments
   # Why some errors? Re-try? Connection issue?
   tryCatch(
     {
       cycleLegs[[i]] <- get_route(i, from, to)
-      print(paste0("Success with id: ", i, " loc: ", from, ", ", to))},
-    error = function(e) {print(paste0("Error with id: ", i, " loc: ", from, ", ", to))})
+      print(paste0("Success: ", 
+                   paste(from, collapse=" "), 
+                   paste(to, collapse=" ")))},
+      error = function(e) {
+        print(paste0("Error: ",
+                     paste(from, collapse=" "),
+                     paste(to, collapse=" ")))})
 }
 
 # Bind individual journies
@@ -128,8 +154,8 @@ mergedPaths <- cyclePaths %>%
   left_join(cycleJourniesSum, by = c("id" = "id"))  
 
 # Save & Load
-#write.csv(mergedPaths, 'route_data.csv', row.names=FALSE)
-mergedPaths <- read.csv('route_data.csv')
+write.csv(mergedPaths, 'route_data2.csv', row.names=FALSE)
+mergedPaths <- read.csv('route_data2.csv')
 
 # Collapse by segments
 dayJournies <- mergedPaths %>% 
@@ -150,9 +176,9 @@ buildingsClip <- gIntersection(buildings, CP, byid=TRUE)
 #Remove axis
 #xquiet<- scale_x_continuous("", breaks=NULL)
 #yquiet<-scale_y_continuous("", breaks=NULL)
-xquiet<-scale_x_continuous(expand=c(0,0), breaks=NULL)
-yquiet<-scale_y_continuous(expand=c(0,0), breaks=NULL)
-quiet<-list(xquiet, yquiet)
+#xquiet<-scale_x_continuous(expand=c(0,0), breaks=NULL)
+#yquiet<-scale_y_continuous(expand=c(0,0), breaks=NULL)
+#quiet<-list(xquiet, yquiet)
 
 # Plot
 ggplot() +
@@ -171,12 +197,31 @@ ggplot() +
   geom_point(data=cycleLocs, aes(x=lon, y=lat),
               colour='white', alpha=0.8, size=0.2)  +  
     
+  # Background blank
   theme(panel.background = element_rect(fill='#2C3539', colour='#2C3539'),
-        legend.position="none") +
-    
-  #http://geepeeex.com/LongitudesAndLatitudes.htm
-  quiet + coord_equal(ratio=122/78) 
-    
+        # Everything apart from panel is blank
+        axis.text.y=element_blank(),
+        panel.margin = unit(c(0,0,0,0), "lines"),
+        axis.text.x=element_blank(),
+        axis.ticks=element_blank(),
+        axis.ticks.length = unit(0,"null"),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.position="none",
+        panel.grid = element_blank(),
+        title = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.margin = unit(0,"null"),
+        plot.margin = rep(unit(0,"null"),4),
+        axis.ticks.length = unit(0,"cm")) +
+  # No labels
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) + 
+  labs(x=NULL, y=NULL, title=NULL) +
+  # Map
+  coord_map(projection = "mercator")
+
 # Save
 ggsave("daily.png", width=20, height=10, dpi=200)
 
@@ -255,4 +300,5 @@ for (hr in levels(factor(hourJournies$hourBin))) {
 
 # GIF
 print("Install ImageMagick")
-system('magick convert  -resize 50% -delay 80 *cycle_journies.png tfl_cycle_hires.gif')
+system('magick convert -delay 80 *cycle_journies.png tfl_cycle_hires.gif')
+
